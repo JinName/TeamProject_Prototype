daf87774cd5ae2a928e0f88517e566434f938adc
SungHo-PC\Sung Ho <kdf4750@naver.com>
2017-05-22 16:23:12 +0900

    ui_update_2

---
+++ Assets/Z_UI/characterfollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterfollow : MonoBehaviour {
+
+    
+    // Use this for initialization
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.y += 0.1f;
+        print(pos);
+    }
+   
+
+}
